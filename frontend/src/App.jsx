9cefd21b48cf208086bb02b844c7b1de17bb092f
Tesfaye <tesfayealemayehu796@gmail.com>
2025-10-16 22:16:03 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login';
+import Doctors from './pages/Doctors';
+import Contact from './pages/Contact';
+import MyAppointments from './pages/MyAppointments';
+import MyProfile from './pages/MyProfile';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:spaciality" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/myappointments" element={<MyAppointments />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment/:docId" element={<About />} />
+      </Routes>
+    </div>
+  );}
+
+export default App
