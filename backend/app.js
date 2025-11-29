d55be7688dc8139aa0780b5ae43901e6bc14d3d5
Tesfaye <tesfayealemayehu796@gmail.com>
2025-11-29 17:54:02 +0300

    conflect

--- backend/app.js
+++ backend/app.js
@@ -1,53 +1,73 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRoute from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
-import userRoute from './routes/userRoute.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRoute from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import userRoute from "./routes/userRoute.js";
 
-// app config 
-const app = express()
-const port = process .env.PORT || 4000
-connectDB()
-connectCloudinary()
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 // middleware
 
-const corsOptions = {
-  origin: [
-    "https://wecarebook.vercel.app",
-    "https://wecareadmin-eight.vercel.app",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  credentials: true,
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-CSRF-Token",
-    "X-Requested-With",
-    "Accept",
-    "Accept-Version",
-    "Content-Length",
-    "Content-MD5",
-    "Date",
-    "X-Api-Version",
-  ],
-};
+// const corsOptions = {
+//   origin: [
+//     "https://wecarebook.vercel.app",
+//     "https://wecareadmin-eight.vercel.app",
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+//   credentials: true,
+//   allowedHeaders: [
+//     "Content-Type",
+//     "Authorization",
+//     "X-CSRF-Token",
+//     "X-Requested-With",
+//     "Accept",
+//     "Accept-Version",
+//     "Content-Length",
+//     "Content-MD5",
+//     "Date",
+//     "X-Api-Version",
+//   ],
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
+const allowedOrigins = [
+  "https://wecarebook.vercel.app",
+
+  "https://wecareadmin-eight.vercel.app",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+    },
+    credentials: true, 
+  })
+);
 app.use(express.json());
-app.options("*", cors(corsOptions));
 
 // api endpoints
- app.use("/api/admin", adminRoute);
-app.use('/api/doctor',doctorRouter)
-app.use("/api/user",userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRouter);
+app.use("/api/user", userRoute);
 
-app.get ('/',(req,res)=>{
-  res.send('Api working')        
-})
+app.get("/", (req, res) => {
+  res.send("Api working");
+});
 
-app.listen(port,()=>{
-  console.log('Server starting',port)        
-})
+app.listen(port, () => {
+  console.log("Server starting", port);
+});
