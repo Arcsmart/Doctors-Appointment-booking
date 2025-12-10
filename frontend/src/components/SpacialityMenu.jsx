995aa06bdfabddb43e621fbd0aa1e14e8551558d
Tesfaye <tesfayealemayehu796@gmail.com>
2025-12-10 19:57:24 +0300

    speciality menu improved

--- frontend/src/components/SpacialityMenu.jsx
+++ frontend/src/components/SpacialityMenu.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { specialityData } from "../assets/data";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 const SpacialityMenu = () => {
